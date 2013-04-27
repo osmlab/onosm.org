9bdeb0aabc2a6c9a36439b4569c5463b7de6cb29
Ian Dees <ian.dees@gmail.com>
2013-04-27 17:59:18 -0500

    Add a map when the address is found.

--- js/site.js
+++ js/site.js
@@ -1,4 +1,8 @@
-var map = L.map('refine-map', { }).setView([37.7, -97.3], 15);
+var findme_map = L.map('findme-map', {zoomControl:false}).setView([37.7, -97.3], 15);
+var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+var osmAttrib='Map data © OpenStreetMap contributors';
+var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 19, attribution: osmAttrib});
+osm.addTo(findme_map);
 
 $("#findme").click(function() {
     var address_to_find = $("#address").val();
@@ -17,9 +21,14 @@ $("#findme").click(function() {
     $("#findme").val("Searching...");
     $.getJSON(url, function(data) {
         if (data.length > 0) {
-            $("#findme").val("Found you!");
+            console.log("Found at least one place.");
+            var chosen_place = data[0];
+
+            findme_map.panTo([chosen_place.lat, chosen_place.lon]);
+            $("#findme-map").show();
+            findme_map.invalidateSize();
         } else {
-            $("#findme").val("We couldn't find that address");
+            console.log("No places found :(");
         }
         console.log(data);
     });
