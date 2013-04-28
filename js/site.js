021e05c75bb01f285f659468743c6a23ab6460d5
Ian Dees <ian.dees@gmail.com>
2013-04-28 12:26:46 -0500

    Add a map with a draggable point and etc.

--- js/site.js
+++ js/site.js
@@ -1,10 +1,16 @@
-var findme_map = L.map('findme-map', {zoomControl:false}).setView([37.7, -97.3], 15);
+var findme_map = L.map('findme-map').setView([37.7, -97.3], 15);
 var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 var osmAttrib='Map data © OpenStreetMap contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 19, attribution: osmAttrib});
+var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 18, attribution: osmAttrib});
 osm.addTo(findme_map);
 
+var findme_marker = L.marker([0,0], {draggable:true});
+findme_marker.addTo(findme_map);
+findme_marker.setOpacity(0);
+
 $("#findme").click(function() {
+    $("#couldnt-find").hide();
+
     var address_to_find = $("#address").val();
 
     if (address_to_find.length === 0) {
@@ -18,18 +24,24 @@ $("#findme").click(function() {
     var url = "http://nominatim.openstreetmap.org/search?" + $.param(qwarg);
     console.log("Nominatim URL is " + url);
 
-    $("#findme").val("Searching...");
+    $("#findme h4").text("Searching...");
+    $("#findme").addClass("loading");
     $.getJSON(url, function(data) {
         if (data.length > 0) {
-            console.log("Found at least one place.");
             var chosen_place = data[0];
+            console.log(chosen_place);
 
-            findme_map.panTo([chosen_place.lat, chosen_place.lon]);
-            $("#findme-map").show();
+            var ll = new L.LatLng(+chosen_place.lat, +chosen_place.lon);
+            findme_marker.setLatLng(ll);
+            findme_marker.setOpacity(1);
+
+            findme_map.panTo(ll);
+            $("#address-step").hide();
+            $("#map-refine-step").show();
             findme_map.invalidateSize();
         } else {
-            console.log("No places found :(");
+            console.log("No data found :(");
+            $("#couldnt-find").show();
         }
-        console.log(data);
     });
 });
