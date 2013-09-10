56ceaeed14543e63a862eaaddd597d88e5fe73d4
Tom MacWright <tom@macwright.org>
2013-09-10 16:13:51 -0400

    little redesigny changes

--- js/site.js
+++ js/site.js
@@ -1,14 +1,14 @@
-var findme_map = L.map('findme-map').setView([37.7, -97.3], 3);
-var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-var osmAttrib='Map data © OpenStreetMap contributors';
-var osm = new L.TileLayer(osmUrl, {minZoom: 2, maxZoom: 18, attribution: osmAttrib});
-osm.addTo(findme_map);
+var findme_map = L.map('findme-map')
+    .setView([37.7, -97.3], 3),
+    osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    osmAttrib = 'Map data © OpenStreetMap contributors',
+    osm = L.tileLayer(osmUrl, {minZoom: 2, maxZoom: 18, attribution: osmAttrib}).addTo(findme_map),
+    category_data = [];
 
-var findme_marker = L.marker([0,0], {draggable:true});
-findme_marker.addTo(findme_map);
+var findme_marker = L.marker([0,0], {draggable:true}).addTo(findme_map);
 findme_marker.setOpacity(0);
 
-var category_data = [];
+if (location.hash) location.hash = '';
 
 $.ajax('./categories.json').success(function(data){
     category_data = data;
@@ -17,33 +17,25 @@ $.ajax('./categories.json').success(function(data){
 $("#category").select2({
     query: function (query) {
         var data = {results: []}, i;
-
         for (i = 0; i < category_data.length; i++) {
             if (query.term.length === 0 || category_data[i].toLowerCase().indexOf(query.term.toLowerCase()) >= 0) {
                 data.results.push({id: category_data[i], text: category_data[i]});
             }
         }
-
         query.callback(data);
     }
 });
 
-$("#findme").click(function() {
+$("#find").submit(function(e) {
+    e.preventDefault();
     $("#couldnt-find").hide();
-
     var address_to_find = $("#address").val();
-
-    if (address_to_find.length === 0) {
-        return;
-    }
-
+    if (address_to_find.length === 0) return;
     var qwarg = {
-        'format': 'json',
-        'q': address_to_find
+        format: 'json',
+        q: address_to_find
     };
     var url = "http://nominatim.openstreetmap.org/search?" + $.param(qwarg);
-    console.log("Nominatim URL is " + url);
-
     $("#findme h4").text("Searching...");
     $("#findme").addClass("loading");
     $.getJSON(url, function(data) {
@@ -51,33 +43,48 @@ $("#findme").click(function() {
             var chosen_place = data[0];
             console.log(chosen_place);
 
-            var bounds = new L.LatLngBounds([+chosen_place.boundingbox[0], +chosen_place.boundingbox[2]],
-                                            [+chosen_place.boundingbox[1], +chosen_place.boundingbox[3]]);
+            var bounds = new L.LatLngBounds(
+                [+chosen_place.boundingbox[0], +chosen_place.boundingbox[2]],
+                [+chosen_place.boundingbox[1], +chosen_place.boundingbox[3]]);
+
             findme_map.fitBounds(bounds);
 
             findme_marker.setOpacity(1);
             findme_marker.setLatLng([chosen_place.lat, chosen_place.lon]);
 
-            $("#instructions").text("We found it! Click and drag the marker to sit on your business.");
-
-            $("#findme h4").text("Done!");
-            $("#findme").removeClass("loading");
-            $("#findme").unbind('click').click(function() {
-                $("#address-step").hide();
-                $("#collect-data-step").show();
-            });
+            $('#instructions').html('We found it! Click and drag the marker to sit on your business, then you are ready to <a href="#details">add details to your business listing</a>.');
+            $('.step-2 a').attr('href', '#details');
         } else {
-            console.log("No data found :(");
-            $("#couldnt-find").show();
-            $("#findme h4").text("Find Me!");
-            $("#findme").removeClass("loading");
+            $('#instructions').html('<strong>We couldn\'t find your address.</strong> Try searching for your street or city without the address.');
         }
+        $("#findme").removeClass("loading");
     });
 });
 
+$(window).on('hashchange', function() {
+    if (location.hash == '#details') {
+        $('#collect-data-step').removeClass('hide');
+        $('#address-step').addClass('hide');
+        $('#confirm-step').addClass('hide');
+        $('.steps').addClass('on-2');
+        $('.steps').removeClass('on-3');
+    } else if (location.hash == '#done') {
+        $('#confirm-step').removeClass('hide');
+        $('#collect-data-step').addClass('hide');
+        $('#address-step').addClass('hide');
+        $('.steps').addClass('on-3');
+    } else {
+        $('#address-step').removeClass('hide');
+        $('#collect-data-step').addClass('hide');
+        $('#confirm-step').addClass('hide');
+        $('.steps').removeClass('on-2');
+        $('.steps').removeClass('on-3');
+    }
+    findme_map.invalidateSize();
+});
+
 $("#collect-data-done").click(function() {
-    $("#collect-data-step").hide();
-    $("#confirm-step").show();
+    location.hash = '#done';
 
     var note_body = "onosm.org submitted note from a business:\n" +
         "name: " + $("#name").val() + "\n" +
