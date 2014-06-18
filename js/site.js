d8030db21088179d5aa195af5e008aa2b1b72e7f
Landon Reed <landontreed@gmail.com>
2014-06-18 10:22:04 -0400

    added subdir for address additions

--- js/site.js
+++ js/site.js
@@ -5,14 +5,20 @@ var findme_map = L.map('findme-map')
     osm = L.tileLayer(osmUrl, {minZoom: 2, maxZoom: 18, attribution: osmAttrib}).addTo(findme_map),
     category_data = [];
 
+// Check if user is on business page or home/address page
+var addr = location.pathname.match(/address/) ? true : false;
+
 var findme_marker = L.marker([0,0], {draggable:true}).addTo(findme_map);
 findme_marker.setOpacity(0);
 
 if (location.hash) location.hash = '';
 
-$.ajax('./categories.json').success(function(data){
-    category_data = data;
-});
+// only need categories.json if finding a business
+if (!addr){
+    $.ajax('/categories.json').success(function(data){
+        category_data = data;
+    });
+}
 
 $("#category").select2({
     query: function (query) {
@@ -51,11 +57,18 @@ $("#find").submit(function(e) {
 
             findme_marker.setOpacity(1);
             findme_marker.setLatLng([chosen_place.lat, chosen_place.lon]);
-
-            $('#instructions').html('We found it! Click and drag the marker to sit on your business, then you are ready to <a href="#details">add details to your business listing</a>.');
+            var successMessage = !addr ? 'We found it! Click and drag the marker to sit on your business, then you are ready to <a href="#details">add details to your business listing</a>.' : 'We found your general vicinity! Click and drag the marker to sit on your home or business, then you are ready to <a href="#details">refine your address</a>.';
+            $('#instructions').html(successMessage);
+            if (addr){
+                $('#address-welcome').html('Ah ha!')
+            }
             $('.step-2 a').attr('href', '#details');
         } else {
-            $('#instructions').html('<strong>We couldn\'t find your address.</strong> Try searching for your street or city without the address.');
+            var failureMessage = !addr ? '<strong>We couldn\'t find your address.</strong> Try searching for your street or city without the address.' : '<strong>We couldn\'t find your street.</strong> Try searching for your city without the address or street.';
+            $('#instructions').html(failureMessage);
+            if (addr){
+                $('#address-welcome').html('Hrrm...')
+            }
         }
         $("#findme").removeClass("loading");
     });
@@ -86,14 +99,21 @@ $(window).on('hashchange', function() {
 $("#collect-data-done").click(function() {
     location.hash = '#done';
 
-    var note_body = "onosm.org submitted note from a business:\n" +
+    var note_body = !addr ? 
+        "onosm.org submitted note from a business:\n" +
         "name: " + $("#name").val() + "\n" +
         "phone: " + $("#phone").val() + "\n" +
         "website: " + $("#website").val() + "\n" +
         "twitter: " + $("#twitter").val() + "\n" +
         "hours: " + $("#opening_hours").val() + "\n" +
         "category: " + $("#category").val() + "\n" +
-        "address: " + $("#address").val(),
+        "address: " + $("#address").val() 
+        : 
+        "onosm.org submitted note for a home address:\n" + 
+        "number: " + $("#number").val() + "\n" +
+        "street: " + $("#street").val() + "\n" +
+        "city: " + $("#city").val() + "\n" +
+        "postal_code: " + $("#postal_code").val() + "\n",
         latlon = findme_marker.getLatLng(),
         qwarg = {
             lat: latlon.lat,
